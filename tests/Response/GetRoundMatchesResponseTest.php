df20b1360d4c04de206b2726b1bcb1ad34f92313
Andrea Stronati <astronati@vendini.com>
2018-02-15 00:23:57 +0100

    resolved #4 Adding getMatches and getRounds to responses (#5)

---
+++ tests/Response/GetRoundMatchesResponseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use SODRP\Response\GetRoundMatchesResponse;
+
+class GetRoundMatchesResponseTest extends TestCase
+{
+    private function getMatchInstance()
+    {
+        $instance = $this->getMockBuilder('SODRP\Model\MatchModel')
+          ->disableOriginalConstructor()
+          ->getMock();
+        return $instance;
+    }
+
+    public function testGetMatches()
+    {
+        $response = [];
+        for ($i = 0; $i < 3; $i++) {
+            $response[] = $this->getMatchInstance();
+        }
+
+        $getRoundMatchesResponse = new GetRoundMatchesResponse($response);
+        $this->assertEquals($response[0], $getRoundMatchesResponse->getMatches()[0]);
+    }
+}
